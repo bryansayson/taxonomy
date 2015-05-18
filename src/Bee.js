2df9134d8543f343e2030f298eb07e0cdb9d1c74
bryan <bry_angelo@yahoo.com>
2015-05-18 12:34:41 -0700

    done

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
 var Bee = function() {
+
+  Grub.call(this);
+// makes Bee a grub
+
+  this.age = 5;
+
+  this.color = 'yellow';
+
+  this.job = 'keep on growing';
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+// set bee prototype to the grub kind
+
+Bee.prototype.constructor = Bee;
